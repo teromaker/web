6cb8600260f05186b1716faf1701f853c3ab62b9
teromaker <144022548+teromaker@users.noreply.github.com>
2023-09-13 07:29:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const contactForm = document.getElementById("contactForm");
 
+    contactForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const nombre = document.getElementById("nombre").value;
+        const email = document.getElementById("email").value;
+        const mensaje = document.getElementById("mensaje").value;
+
+        // Aquí puedes agregar la lógica para enviar el formulario o realizar alguna acción con los datos.
+        // Por ahora, solo mostraremos un mensaje en la consola.
+        console.log("Nombre:", nombre);
+        console.log("Email:", email);
+        console.log("Mensaje:", mensaje);
+
+        // Limpia el formulario después de enviarlo
+        contactForm.reset();
+    });
+});
